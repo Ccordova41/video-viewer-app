942cec3d3c44df2ebb0aa19fd67f333d808e2290
Ccordova41 <ccordova41@gmail.com>
2018-02-17 13:12:06 -0500

    added create action

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -3,4 +3,20 @@ class API::VideosController < ApplicationController
     videos = Video.all
     render json: videos, status: 200
   end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video, status: 201
+    end
+  end
+
+  private
+  
+  def video_params
+    params.require(:video).permit(:title, :description, :youtube_video_id)
+  end
+
+
+
 end
