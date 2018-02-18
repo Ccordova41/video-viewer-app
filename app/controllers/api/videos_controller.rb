7ccf038d3afd9637002aae88a3ca0bff3e8a5801
Ccordova41 <ccordova41@gmail.com>
2018-02-18 00:01:04 -0500

    add the GET /api/videos/:id functionality

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -8,11 +8,30 @@ class API::VideosController < ApplicationController
     video = Video.new(video_params)
     if video.save
       render json: video, status: 201
+    else
+      render json: {
+          errors: {
+            messages: video.errors.messages
+          }
+      }, status: 422
     end
   end
 
+  def show
+    video = Video.find_by(id: params[:id])
+    if video
+      render json: video, status: 200
+    else
+      render json: {
+        errors: {
+          messages: { video: "Can't be found" }
+        }
+      }, status: 404
+  end
+end
+
   private
-  
+
   def video_params
     params.require(:video).permit(:title, :description, :youtube_video_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :videos, only: [:index]
-  end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :videos, only: [:index, :create, :show]
+  end
 end
