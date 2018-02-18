f5beb44a54ae8f60561fe332305083c5d44b6d64
Ccordova41 <ccordova41@gmail.com>
2018-02-18 09:45:49 -0500

    added update, destroy actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :videos, only: [:index, :create, :show]
+    resources :videos, only: [:index, :create, :show, :update. :destroy]
   end
 end
